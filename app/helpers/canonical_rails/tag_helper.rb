76c22d0723f5e3437da2258afe0fa2b75b0d670e
Graham Swan <thinkswan@gmail.com>
2014-03-11 17:11:56 -0600

    output multiple whitelisted params as '?key1=val1&key2=val2' (not '?key1=val1=key2=val2')

--- app/helpers/canonical_rails/tag_helper.rb
+++ app/helpers/canonical_rails/tag_helper.rb
@@ -1,10 +1,10 @@
 module CanonicalRails
   module TagHelper
-    
+
     def trailing_slash_needed?
       CanonicalRails.sym_collection_actions.include? request.params['action'].to_sym
     end
-    
+
     def trailing_slash_if_needed
       "/" if trailing_slash_needed? and request.path != '/'
     end
@@ -12,28 +12,28 @@ module CanonicalRails
     def path_without_html_extension
       request.path.sub(/\.html$/, '')
     end
-    
+
     def canonical_host
       CanonicalRails.host || request.host
     end
-    
+
     def canonical_href(host=canonical_host)
       "#{request.protocol}#{host}#{path_without_html_extension}#{trailing_slash_if_needed}#{whitelisted_query_string}"
     end
-    
+
     def canonical_tag(host=canonical_host)
-      tag(:link, :href => canonical_href(host), :rel => 'canonical')
+      tag(:link, :href => raw(canonical_href(host)), :rel => 'canonical')
     end
-    
+
     def whitelisted_params
       request.params.select do |key, value|
         value.present? and
         CanonicalRails.sym_whitelisted_parameters.include? key.to_sym
       end
     end
-    
+
     def whitelisted_query_string
-      "?#{whitelisted_params.to_a.join('=')}" if whitelisted_params.present?
+      "?#{whitelisted_params.map{ |key, val| "#{key}=#{val}" }.join('&')}" if whitelisted_params.present?
     end
   end
 end
