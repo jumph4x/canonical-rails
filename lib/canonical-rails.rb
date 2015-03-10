c843ff9e67bb6389ecd7202f21c0205e5a3b26d2
John Riviello <github@riviello.net>
2015-03-10 14:18:35 -0400

    Adding a config option to force the protocol
    This fixes Issue #13

--- lib/canonical-rails.rb
+++ lib/canonical-rails.rb
@@ -6,29 +6,32 @@ module CanonicalRails
   # a fresh initializer with all configuration values.
   #
   # the config\setup concept politely observed at and borrowed from Devise: https://github.com/plataformatec/devise/blob/master/lib/devise.rb
-  
+
   def self.setup
     yield self
   end
 
   mattr_accessor :host
   @@host = nil
-  
+
+  mattr_accessor :protocol
+  @@protocol = nil
+
   mattr_accessor :collection_actions
   @@collection_actions = [:index]
-  
+
   mattr_accessor :member_actions
   @@member_actions = [:show]
-  
+
   mattr_accessor :whitelisted_parameters
   @@whitelisted_parameters = []
 
   def self.sym_collection_actions
     @@sym_collection_actions ||= self.collection_actions.map(&:to_sym)
   end
-  
+
   def self.sym_whitelisted_parameters
     @@sym_whitelisted_actions ||= self.whitelisted_parameters.map(&:to_sym)
   end
-  
+
 end
