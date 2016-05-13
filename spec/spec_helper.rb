d72c749ee60250882d94d418e86752257777bf39
Krzysztof Rybka <krzysztof.rybka@gmail.com>
2016-05-13 23:37:25 +0200

    add pry to dev dependencies

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment.rb", __FILE__)
 require 'rspec/rails'
+require 'pry'
 
 Rails.backtrace_cleaner.remove_silencers!
 
