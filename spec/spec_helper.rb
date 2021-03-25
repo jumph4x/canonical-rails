1b9426229dfa5136326f5ce4963e8e8d1cb3b23c
Phil Lee <asmega@gmail.com>
2021-03-25 12:36:48 +0000

    Remove rails gem therefore mimemagic gem
    - This is due to licensing issues
    - Further discussion can be found at https://github.com/rails/rails/issues/41750

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,3 @@ Rails.backtrace_cleaner.remove_silencers!
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-
-RSpec.configure do |config|
-  config.use_transactional_fixtures = true
-end
